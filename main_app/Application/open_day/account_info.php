72f7914a1114a6c367fdfc9288ea2209daa6dd13
irfanzaman511 <I.U.Zaman@wlv.ac.uk>
2025-05-14 09:01:11 +0100

    Add files via upload

--- main_app/Application/open_day/account_info.php
+++ main_app/Application/open_day/account_info.php
@@ -14,7 +14,6 @@ $email = htmlspecialchars($_SESSION['email']);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Floating button style */
@@ -58,23 +57,6 @@ $email = htmlspecialchars($_SESSION['email']);
     </style>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Welcome, <?php echo $username; ?></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ms-auto">
-        <a class="nav-link active" href="#">Home</a>
-        <a class="nav-link disabled" href="#" tabindex="-1"><?php echo $email; ?></a>
-      </div>
-    </div>
-  </div>
-</nav>
 
 <!-- Floating Button -->
 <button class="floating-btn" onclick="toggleAccountInfo()">👤</button>
